4f46892111ac6e8098ff9f4148b5454d1716c28e
mattleec <matthewclee02@gmail.com>
2024-03-09 04:59:44 -0500

    testing

--- script.js
+++ script.js
@@ -146,6 +146,165 @@
 
 
 
+// const imgDFR = 380
+// const spinner = document.querySelector(".spinner");
+// const skillLogos = spinner.querySelectorAll('img');
+
+// // function skillTransition() {
+// //     var navUA = navigator.userAgent.toLowerCase(); 
+// //     var checkSafari = false;
+// //     try {
+// //         checkSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+// //     } catch(err) {}
+// //     checkSafari = (checkSafari || ((navUA.indexOf('safari') != -1)&& (!(navUA.indexOf('chrome')!= -1) && (navUA.indexOf('version/')!= -1))));
+
+// //     if (checkSafari) {
+// //         skillLogos.forEach((skills, index) => {
+// //             skills.style.transition = "transform 1s, opacity 1s";
+// //             skills.style.transform = "rotateY(" + (index * (360 / skillLogos.length)) + "deg) translateZ(" + imgDFR + "px)";
+// //             skills.style.opacity = "1";
+// //             skills.style.transitionDelay = ".500s";
+// //         });
+// //     } else {
+// //         skillLogos.forEach((skills, index) => {
+// //             skills.style.transition = "transform 1s, opacity 1s";
+// //             skills.style.transform = "rotateY(" + (index * (360 / skillLogos.length)) + "deg) translateZ(" + imgDFR + "px)";
+// //             skills.style.opacity = "1";
+// //             skills.classList.add('imgReflect');
+// //             skills.style.transitionDelay = ".500s";
+// //         });
+// //     }
+// //     initResponsive();
+// // }
+
+// function skillTransition() {
+//     var navUA = navigator.userAgent.toLowerCase(); 
+//     var checkSafari = false;
+//     try {
+//         checkSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+//     } catch(err) {}
+//     checkSafari = (checkSafari || ((navUA.indexOf('safari') != -1)&& (!(navUA.indexOf('chrome')!= -1) && (navUA.indexOf('version/')!= -1))));
+
+//     setTimeout(() => {
+//         skillLogos.forEach((skills, index) => {
+//             skills.style.transition = "transform 1s, opacity 1s";
+//             skills.style.transform = `rotateY(${index * (360 / skillLogos.length)}deg) translateZ(${imgDFR}px)`;
+//             skills.style.opacity = "1";
+//             if (!checkSafari) {
+//                 skills.classList.add('imgReflect');
+//             }
+//             skills.style.transitionDelay = ".500s";
+//         });
+//         initResponsive();
+//     }, 50);
+// }
+
+// function initResponsive() {
+//     let screenWidth = window.innerWidth;
+//     let translateZValue = Math.min(screenWidth / 3, imgDFR);
+//     let newSize;
+
+//     if (screenWidth <= 574) {
+//         newSize = Math.max(50, 100 - (574 - screenWidth) / 5.24);
+//     } else {
+//         newSize = 100;
+//     }
+
+//     spinner.style.width = newSize + "px";
+//     spinner.style.height = newSize + "px";
+
+//     skillLogos.forEach((skills, index) => {
+//         skills.style.transform = "rotateY(" + (index * (360 / skillLogos.length)) + "deg) translateZ(" + translateZValue + "px)";
+//     });
+// }
+
+// function responsive() {
+//     let screenWidth = window.innerWidth;
+//     let translateZValue = Math.min(screenWidth / 3, imgDFR);
+//     let newSize;
+
+//     if (screenWidth <= 574) {
+//         newSize = Math.max(50, 100 - (574 - screenWidth) / 5.24);
+//     } else {
+//         newSize = 100;
+//     }
+
+//     spinner.style.width = newSize + "px";
+//     spinner.style.height = newSize + "px";
+
+//     if (skillLogos[0].style.transitionDelay != ".0s") {
+//         skillLogos.forEach((skills, index) => {
+//             skills.style.transform = "rotateY(" + (index * (360 / skillLogos.length)) + "deg) translateZ(" + translateZValue + "px)";
+//             skills.style.transitionDelay = ".0s";
+//         });
+//     } else {
+//         skillLogos.forEach((skills, index) => {
+//             skills.style.transform = "rotateY(" + (index * (360 / skillLogos.length)) + "deg) translateZ(" + translateZValue + "px)";
+//         });
+//     }
+// }
+
+// window.onload = function() {
+//     skillTransition();
+//     // skillTransition;
+// };
+
+// // document.addEventListener("DOMContentLoaded", function(arg) {
+// //     // do something cool
+// //     skillTransition();
+// // });
+
+// // window.onload = function(){
+// //     window.document.body.onload = skillTransition; // note removed parentheses
+// // };
+
+// window.addEventListener('resize', responsive);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const observer = new IntersectionObserver((entries, observer) => {
+//         const entry = entries[0];
+//         if (entry.isIntersecting) {
+//             entry.target.querySelectorAll('.no-animation').forEach(element => {
+//                 element.classList.remove('no-animation');
+//             });
+//             observer.unobserve(entry.target);
+//         }
+//     }, {
+//         rootMargin: '0px',
+//         threshold: 0.2
+//     });
+
+//     const section = document.querySelector('.timelineInit');
+//     observer.observe(section);
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var testers = document.querySelectorAll('.skillName');
+
+//     testers.forEach(function(tester) {
+//         tester.addEventListener('mouseenter', function() {
+//             var matchingTesters = document.querySelectorAll('.skillName[data-name="' + tester.getAttribute('data-name') + '"]');
+//             matchingTesters.forEach(function(t) {
+//                 t.classList.add('skillName-hovered');
+//             });
+//         });
+
+//         tester.addEventListener('mouseleave', function() {
+//             var matchingTesters = document.querySelectorAll('.skillName[data-name="' + tester.getAttribute('data-name') + '"]');
+//             matchingTesters.forEach(function(t) {
+//                 t.classList.remove('skillName-hovered');
+//             });
+//         });
+//     });
+// });
+
+
+
+
+
+
+
+
 const imgDFR = 380
 const spinner = document.querySelector(".spinner");
 const skillLogos = spinner.querySelectorAll('img');
@@ -185,18 +344,16 @@ function skillTransition() {
     } catch(err) {}
     checkSafari = (checkSafari || ((navUA.indexOf('safari') != -1)&& (!(navUA.indexOf('chrome')!= -1) && (navUA.indexOf('version/')!= -1))));
 
-    setTimeout(() => {
-        skillLogos.forEach((skills, index) => {
-            skills.style.transition = "transform 1s, opacity 1s";
-            skills.style.transform = `rotateY(${index * (360 / skillLogos.length)}deg) translateZ(${imgDFR}px)`;
-            skills.style.opacity = "1";
-            if (!checkSafari) {
-                skills.classList.add('imgReflect');
-            }
-            skills.style.transitionDelay = ".500s";
-        });
-        initResponsive();
-    }, 50);
+    skillLogos.forEach((skills, index) => {
+        skills.style.transition = "transform 1s, opacity 1s";
+        skills.style.transform = `rotateY(${index * (360 / skillLogos.length)}deg) translateZ(${imgDFR}px)`;
+        skills.style.opacity = "1";
+        if (!checkSafari) {
+            skills.classList.add('imgReflect');
+        }
+        skills.style.transitionDelay = ".500s";
+    });
+    initResponsive();
 }
 
 function initResponsive() {
@@ -244,10 +401,10 @@ function responsive() {
     }
 }
 
-window.onload = function() {
-    skillTransition();
-    // skillTransition;
-};
+// window.onload = function() {
+//     skillTransition();
+//     // skillTransition;
+// };
 
 // document.addEventListener("DOMContentLoaded", function(arg) {
 //     // do something cool
@@ -258,7 +415,7 @@ window.onload = function() {
 //     window.document.body.onload = skillTransition; // note removed parentheses
 // };
 
-window.addEventListener('resize', responsive);
+// window.addEventListener('resize', responsive);
 
 document.addEventListener('DOMContentLoaded', function() {
     const observer = new IntersectionObserver((entries, observer) => {
@@ -297,3 +454,12 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+window.addEventListener ? 
+    window.addEventListener("load",skillTransition,false) 
+    : 
+    window.attachEvent && window.attachEvent("onload",skillTransition);
+
+window.addEventListener('resize', responsive);
